32ab00723415effb5bbeec7ca215fcd6a16aebb2
Pramod <Pramod@LAPTOP-9004U6SO>
2022-04-10 12:47:19 +0530

    first commit

---
+++ src/com/xyz/TestGit.java
@@ -0,0 +1,20 @@
+package com.xyz;
+
+public class TestGit {
+	public static void main(String[] args) {
+		
+		
+		//rhight click on project -->teams -->share project--> it will create local repo
+		//rightclick-->teams-->add to index-->it will add your prject  to staging area 
+		//right click -->commit the code
+		//right click on project-->team-->remote-->push-->add url-->provide user and password
+		
+		System.out.println("java");
+		
+		System.out.println("selenium");
+		
+		System.out.println("python");
+		
+	}
+
+}
